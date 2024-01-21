03e51bb65b80b2bb532bf840759a8d0d57771ea3
KAVYAE1610 <kavyasheela1610@gmail.com>
2024-01-21 22:12:29 +0530

    this is my third commiti

---
+++ problem2.js
@@ -0,0 +1,8 @@
+a='a'
+console.log("General variable declaration aa'",a)
+var b='var'
+console.log("this variable belongs to var type",b)
+let c='let'
+console.log("this variable belongs to led type",c)
+const d=100                                          
+console.log("this variable belongs to const type",d)
---
+++ problem3.js
@@ -0,0 +1,28 @@
+var a=10
+
+console.log(a)
+
+//arithmetic operator
+
+var b=20
+
+console.log("Arithemetic operator addition:",a+b)
+console.log("Arithemetic operator subtraction:",a-b)
+console.log("Arithemetic operator multiplication:",a*b)
+console.log("Arithemetic operator division:",a/b)
+console.log("Arithemetic operator modulus :",a%b)
+console.log(typeof(a))
+
+//logical opeartor
+
+console.log("logical operator and :",a||b)
+console.log("logical operator or:",a&&b)
+console.log("logical operator not:",!a)
+
+console.log(10&&30)
+console.log(30&&10)
+console.log(10||30)
+console.log(30||10)
+console.log(0||10)
+console.log(!10)
+console.log(!-20)
